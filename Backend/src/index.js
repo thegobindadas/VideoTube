3b3d6e76ede764a9196090b960c31a466f514259
Gobinda Das <dasgobinda9955@gmail.com>
2024-09-19 22:34:40 +0530

    add database configuration

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,8 +1,24 @@
 import dotenv from "dotenv"
-
+import mongoose from "mongoose";
+import config from "./config/index.js";
+import { DB_NAME } from './constants.js'
 
 
 
 dotenv.config({
     path: './.env'
 })
+
+
+
+
+
+;(async () => {
+    try {
+        await mongoose.connect(`${config.MONGODB_URI}/${DB_NAME}`);
+
+    } catch (error) {
+        console.log("MongoDB connection error", error);
+        throw error
+    }
+})()
