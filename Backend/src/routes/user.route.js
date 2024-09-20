2fed5b2a12424a3eaa548a667194856321cd2b30
Gobinda Das <dasgobinda9955@gmail.com>
2024-09-20 20:45:01 +0530

    add register controller

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { upload } from "../middlewares/multer.middleware.js"
 import { registerUser } from "../controllers/user.controller.js"
 
 
@@ -6,7 +7,19 @@ const router = Router()
 
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 
 
 
