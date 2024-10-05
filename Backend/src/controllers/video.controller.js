487b06526dc57833857f84c7c256657766c3054e
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-10-05 09:26:21 +0530

    update video controller

--- Backend/src/controllers/video.controller.js
+++ Backend/src/controllers/video.controller.js
@@ -1,6 +1,6 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import mongoose, { isValidObjectId } from "mongoose"
 import { Video } from "../models/video.model.js";
-import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadOnCloudinary, deletePhotoOnCloudinary, deleteVideoOnCloudinary, deleteFolderOnCloudinary } from "../utils/cloudinary.js";
