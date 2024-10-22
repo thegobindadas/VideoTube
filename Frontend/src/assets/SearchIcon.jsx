d73dff58874247dc78dc0aac53dca9d14d37211c
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-10-22 12:17:04 +0530

    update SearchIcon

--- Frontend/src/assets/SearchIcon.jsx
+++ Frontend/src/assets/SearchIcon.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function SearchIcon({className="h-4 w-4"}) {
+function SearchIcon({className="h-4 w-4", strokeWidth="1.5"}) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth="1.5"
+      strokeWidth={strokeWidth}
       stroke="currentColor"
       aria-hidden="true"
       className={className}>
