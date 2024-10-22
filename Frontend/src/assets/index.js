dc5a4a10d4edf337aab12e8b7cb91248ecf61727
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-10-22 11:58:22 +0530

    Add Icons

---
+++ Frontend/src/assets/index.js
@@ -0,0 +1,37 @@
+import LogoIcon from "./LogoIcon";
+import SearchIcon from "./SearchIcon";
+import CloseIcon from "./CloseIcon";
+import LikedVideoIcon from "./LikedVideoIcon";
+import ContentIcon from "./ContentIcon";
+import SupportIcon from "./SupportIcon";
+import SettingIcon from "./SettingIcon";
+import HomeIcon from "./HomeIcon";
+import HistoryIcon from "./HistoryIcon";
+import CollectionIcon from "./CollectionIcon";
+import SubscriberIcon from "./SubscriberIcon";
+import VideoIcon from "./VideoIcon";
+import TweetIcon from "./TweetIcon";
+import TweetLikeIcon from "./TweetLikeIcon";
+import TweetDislikeIcon from "./TweetDislikeIcon";
+import CheckboxIcon from "./CheckboxIcon";
+
+
+
+export {
+    LogoIcon,
+    SearchIcon,
+    CloseIcon,
+    LikedVideoIcon,
+    ContentIcon,
+    SupportIcon,
+    SettingIcon,
+    HomeIcon,
+    HistoryIcon,
+    CollectionIcon,
+    SubscriberIcon,
+    VideoIcon,
+    TweetIcon,
+    TweetLikeIcon,
+    TweetDislikeIcon,
+    CheckboxIcon,
+}
