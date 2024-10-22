64040057040b205a6696af857c667262625eb035
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-10-22 20:10:22 +0530

    Add router

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+import SignupPage from './pages/SignupPage.jsx'
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <SignupPage />,
+      },
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
