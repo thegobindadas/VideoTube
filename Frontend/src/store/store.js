6d25c17b51cd717185fdf3dce74af91760ae586c
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-11-08 20:11:24 +0530

    add comment slice

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import userSlice from "./userSlice"
 import videoSlice from "./videoSlice"
 import watchHistorySlice from "./watchHistorySlice"
+import commentSlice from "./commentSlice"
 
 
 const store = configureStore({
@@ -9,6 +10,7 @@ const store = configureStore({
         user : userSlice,
         videos: videoSlice,
         watchHistory: watchHistorySlice,
+        comments: commentSlice,
     }
 });
 
