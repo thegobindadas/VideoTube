a9b948efb5595fa6b252da854fed6f5e1de6a064
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-11-26 23:16:58 +0530

    update Button component

--- Frontend/src/components/Buttons/Button.jsx
+++ Frontend/src/components/Buttons/Button.jsx
@@ -5,13 +5,15 @@ export default function Button({
     type = "button",
     bgColor = "",
     textColor = "",
+    width="w-full",
     className = "p-3",
     ...props
 }) {
     return (
-        <button className={`w-full bg-[#ae7aff] text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]
+        <button className={`bg-[#ae7aff] text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]
          ${bgColor} 
-         ${textColor} 
+         ${textColor}
+         ${width}
          ${className}`} 
          {...props}
          >
