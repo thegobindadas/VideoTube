303d6c16470823d14b717266092cf7d5bccefce6
Gobinda Das <103523606+thegobindadas@users.noreply.github.com>
2024-11-28 01:01:15 +0530

    add formatFileSize utility function

---
+++ Frontend/src/utils/fileSizeUtils.js
@@ -0,0 +1,13 @@
+export const formatFileSize = (sizeInBytes) => {
+    if (sizeInBytes < 1024) {
+        return `${sizeInBytes} Bytes`; // Less than 1 KB
+    } else if (sizeInBytes < 1024 * 1024) {
+        return `${(sizeInBytes / 1024).toFixed(2)} KB`; // Less than 1 MB
+    } else if (sizeInBytes < 1024 * 1024 * 1024) {
+        return `${(sizeInBytes / (1024 * 1024)).toFixed(2)} MB`; // Less than 1 GB
+    } else {
+        return `${(sizeInBytes / (1024 * 1024 * 1024)).toFixed(2)} GB`; // Greater than or equal to 1 GB
+    }
+};
+
+  
